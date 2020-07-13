97f8dbbb9c643034770155c91807f24e84841e19
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-07-13 15:40:07 +0600

    add model

--- GraphQL/GraphQLBlogPost/GraphQLBlogPost/Startup.cs
+++ GraphQL/GraphQLBlogPost/GraphQLBlogPost/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphiQl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,7 +44,7 @@ namespace GraphQLBlogPost
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseGraphiQl("/graphql");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
